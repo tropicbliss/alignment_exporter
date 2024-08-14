e252b5844b0651849b599e6c39b7d807adbab474
tropicbliss <tropicbliss@protonmail.com>
2024-08-14 17:19:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# alignment-exporter
+
+This crate provides alignment info for a user-defined struct, though this crate has increased in scope over time to include other information, such as the type name of struct fields.
+
+Using this crate requires a MSRV of 1.80.
+
+## Example
+```rs
+use alignment_exporter_derive::export_alignment;
+
+// `export_alignment` already annotates #[repr(C)] to the struct, so adding that yourself is not required. However, it is always better to include it in your code for the sake of explicitness.
+#[export_alignment]
+struct Example {
+    a: u8,
+    b: u32,
+    c: u16
+}
+
+fn main() {
+    let alignment = Example::get_alignment();
+    assert_eq!(alignment, vec![
+        Alignment { size: 1, offset: 0, ty_name: "u8" },
+        Alignment { size: 4, offset: 1, ty_name: "u32" },
+        Alignment { size: 2, offset: 8, ty_name: "u16" },
+    ]);
+}
+```
---
+++ alignment_exporter_derive/src/lib.rs
@@ -0,0 +1,65 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
+
+/// Any struct that uses this procedural macro is automatically annotated with `#[repr(C)]`. Learn more about the significance of this annotation and why it is required [here](https://doc.rust-lang.org/nomicon/other-reprs.html#reprc).
+#[proc_macro_attribute]
+pub fn export_alignment(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as DeriveInput);
+    let struct_name = &input.ident;
+    let data = match &input.data {
+        Data::Struct(data) => data,
+        _ => {
+            return TokenStream::from(quote! {
+                compile_error!("export_alignment can only be used on structs");
+            });
+        }
+    };
+    let mut types = Vec::new();
+    if let syn::Fields::Named(ref fields) = data.fields {
+        for field in &fields.named {
+            let ty = &field.ty;
+            types.push(quote! {{
+                let size = std::mem::size_of::<#ty>();
+                let alignment = std::mem::align_of::<#ty>();
+                vec.push(Alignment {
+                    size,
+                    offset,
+                    ty_name: stringify!(#ty)
+                });
+                if alignment > max_alignment {
+                    max_alignment = alignment;
+                }
+                if offset % alignment != 0 {
+                    let padding = alignment - (offset % alignment);
+                    offset += padding;
+                }
+                let field_offset = offset;
+                offset += size;
+            }});
+        }
+    }
+    let output = quote! {
+        #[repr(C)]
+        #input
+
+        use ::std::sync::LazyLock;
+        use ::std::vec::Vec;
+        use ::alignment_exporter::{Alignment, AlignmentExporter};
+
+        impl AlignmentExporter for #struct_name {
+            fn get_alignment() -> &'static [Alignment] {
+                static RESULT: LazyLock<Vec<Alignment>> = LazyLock::new(|| {
+                    let mut vec = Vec::new();
+                    let mut offset = 0;
+                    let mut max_alignment = 0;
+                    #(#types)*
+
+                    vec
+                });
+                RESULT.as_slice()
+            }
+        }
+    };
+    output.into()
+}
